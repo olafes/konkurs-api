e8da47a1bb41f2bbbc87998866f0ffbf9719e367
Olaf Siwinski <olafsiwinski@gmail.com>
2019-10-24 22:49:48 +0200

    aa

--- config/config-dev.js
+++ config/config-dev.js
@@ -5,6 +5,6 @@ module.exports = {
   saltRounds: 10,
   MongoURI: 'mongodb://localhost:27017/konkurs',
   google: {
-    clientID: '765380157329-qkpmq9v5f32m71ca3vopi6m031s2q69u.apps.googleusercontent.com'
+    clientID: '490869806290-17bk9lor8fcb091vni1kvanqqjh6o3up.apps.googleusercontent.com'
   }
 };
--- config/config-production.js
+++ config/config-production.js
@@ -3,5 +3,8 @@ module.exports = {
   port: 8080,
   sessionSecret: process.env.SESSION_SECRET,
   saltRounds: 10,
-  MongoURI: `mongodb+srv://${process.env.DB_LOGIN}:${process.env.DB_PASSWORD}@cluster0-2kr8u.mongodb.net/${process.env.DB_NAME}?retryWrites=true&w=majority`
+  MongoURI: `mongodb+srv://${process.env.DB_LOGIN}:${process.env.DB_PASSWORD}@cluster0-2kr8u.mongodb.net/${process.env.DB_NAME}?retryWrites=true&w=majority`,
+  google: {
+    clientID: '490869806290-17bk9lor8fcb091vni1kvanqqjh6o3up.apps.googleusercontent.com'
+  }
 };
